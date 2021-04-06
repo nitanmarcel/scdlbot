d42a971a9434ba765b102310d6bd08346600bb4a
Nitan Alexandru Marcel <nitan.marcel@protonmail.com>
2021-04-06 13:16:13 +0300

    Update utils.py

--- scdlbot/utils.py
+++ scdlbot/utils.py
@@ -62,7 +62,7 @@ def get_direct_urls(url, cookies_file=None, cookies_download_file=None, source_i
 
     youtube_dl_args.extend(["--get-url", url])
     youtube_dl_args.extend(["--rm-cache-dir"])
-    youtube_dl_bin["--rm-cache-dir"].run(timeout=60)
+    #youtube_dl_bin["--rm-cache-dir"].run(timeout=60)
     try:
         ret_code, std_out, std_err = youtube_dl_bin[youtube_dl_args].run(timeout=60)
     except ProcessTimedOut as exc:
@@ -156,4 +156,5 @@ def get_link_text(urls):
                         content_type = "Video"
                 # direct_url = shorten_url(direct_url)
                 link_text += "• {} [Direct Link]({})\n".format(content_type, direct_url)
+    link_text += "\n*NOTE* Final download URLs are only guaranteed to work on the same machine/IP where extracted"
     return link_text
